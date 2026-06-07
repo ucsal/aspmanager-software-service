a95a54200c5b3cc0cf24a89bdb118289d17a92e0
GabrielFraga <87135402+Gab1203@users.noreply.github.com>
2026-06-07 16:31:16 -0300

    fix: Add OpenAPI server annotation
    Add OpenAPI server annotation to SoftwareApplication.

--- src/main/java/com/ucsal/software/SoftwareApplication.java
+++ src/main/java/com/ucsal/software/SoftwareApplication.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
 @OpenAPIDefinition(
