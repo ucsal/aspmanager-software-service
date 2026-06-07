45251f8f3af79d09dfa47ca6bc44abe4a6867a32
SilvaOrdep <pedro.o.silva975@gmail.com>
2026-06-07 17:30:59 -0300

    fix(swagger): add missing SecurityScheme and SecurityRequirement for swagger UI

--- src/main/java/com/ucsal/software/SoftwareApplication.java
+++ src/main/java/com/ucsal/software/SoftwareApplication.java
@@ -5,6 +5,10 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
@@ -12,11 +16,19 @@ import io.swagger.v3.oas.annotations.servers.Server;
     info = @Info(
     title = "ASPManager API - Softwares", version = "1.0", 
     description = "Microserviço de Gestão e Solicitação de Softwares"),
-    
+    security = @SecurityRequirement(name = "bearerAuth"),
     servers = {
         @Server(url = "http://localhost:8085", description = "Ambiente Local (Desenvolvimento)"),
-        @Server(url = "http://localhost:8080/api/v1/orq/software", description = "API Gateway (Produção)")
+        @Server(url = "http://localhost:8080", description = "API Gateway (Produção)")
     })
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT Token access",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
 @EnableDiscoveryClient
 public class SoftwareApplication {
 
